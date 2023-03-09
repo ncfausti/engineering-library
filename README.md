bc722d54e5912e7aeed64b055bb7001b933e058a
Nick Fausti <ncfausti@gmail.com>
2023-03-09 16:43:29 -0500

    Add css flexbox full width stretch (grow and shrink)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # engineering-library
 A library of code to help with random software engineering work.
+
+TODO: Fine-tune LLM on code base to be able to return projects/snippets I've worked on previously.
--- list-processing/index.js
+++ list-processing/index.js
@@ -1,16 +1,37 @@
-const eng = ["area", "arrive", "hello", "halo", "help", "okay", ];
-const it = ["ciao", "buonasera", "arrivederci"];
-const langs = [eng, it];
-
-document.addEventListener('DOMContentLoaded', function() {
-  const inp = document.querySelector("#multi-lang-autocomplete");
-  inp.addEventListener('keyup', onKeyUpHandler);
-});
 
 // compare value of input against all words in a list
-export const matches = (inputString, ls) => ls.filter(word => word.startsWith(inputString));
-export const checkAllLanguages = (languagesList, inputString) => languagesList.map(lang => matches(inputString, lang ));
+const matches = (inputString, ls) => ls.filter(word => word.startsWith(inputString));
+const checkAllLanguages = (languagesList, inputString) => languagesList.map(lang => matches(inputString, lang ));
 
-function onKeyUpHandler(e) {
-	console.log(checkAllLanguages(langs, e.target.value));
+// function to compute the levenshtein distance between two strings (used for autocomplete)
+const levenshtein = (a, b) => {
+  if (a.length === 0) return b.length;
+  if (b.length === 0) return a.length;
+
+  const matrix = [];
+
+  // increment along the first column of each row
+  for (let i = 0; i <= b.length; i++) {
+    matrix[i] = [i];
+  }
+
+  // increment each column in the first row
+  for (let j = 0; j <= a.length; j++) {
+    matrix[0][j] = j;
+  }
+
+  // fill in the rest of the matrix
+  for (let i = 1; i <= b.length; i++) {
+    for (let j = 1; j <= a.length; j++) {
+      if (b.charAt(i - 1) === a.charAt(j - 1)) {
+        matrix[i][j] = matrix[i - 1][j - 1];
+      } else {
+        matrix[i][j] = Math.min(matrix[i - 1][j - 1] + 1, // substitution
+          Math.min(matrix[i][j - 1] + 1, // insertion
+            matrix[i - 1][j] + 1)); // deletion
+      }
+    }
+  }
+
+  return matrix[b.length][a.length];
 }
